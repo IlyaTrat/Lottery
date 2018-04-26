a3e9db42b9fc762a2db64bba8818ed8a7b5f4ddc
Трацевский Илья <deeo-sama@hotmail.com>
2018-04-26 21:33:30 +0300

    random & send

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class BuyComponent implements OnInit {
   private balls = [];
   private maxSelectedCheckbox = 4;
+  private maxBalls = 50;
   constructor() {
   }
 
@@ -17,12 +18,34 @@ export class BuyComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.fillList(50);
+    this.fillList(this.maxBalls);
   }
 
   private fillList(x: number) {
     for (let i = 0; i < x; i++) {
       this.balls.push({ id: i, value: false });
     }
+  }
+
+  private selectRandomBalls (x: number) {
+    const rand = [];
+    this.balls.map(item => item.value = false);
+
+    while (rand.length < x) {
+      let temp = this.getRandomInt(0, this.maxBalls);
+      rand.includes(temp) ? temp = 0 : rand.push(temp);
+    }
+
+    return rand.forEach(item => this.balls[item].value = true);
+  }
+
+// TODO: add service
+  private sendChosenBalls () {
+    const send = this.balls.filter(item => item.value === true);
+    send.length !== 4 ? console.log('Wrong number') : console.log(send);
+  }
+
+  private getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 }
